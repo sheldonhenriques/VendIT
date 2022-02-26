5c22e261a0fed473ec46acc921ac2b94665eaede
sheldon <sheldonhenriques2727@gmail.com>
2022-02-27 01:32:09 +0530

    updated contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,22 @@
 require('babel-register');
 require('babel-polyfill');
-
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "ankle rent assist adult spread tired office magnet episode sword job rough";
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
+    develop : {
+      host: "127.0.0.10",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*",
     },
+    ropsten: {
+      provider:function(){
+      return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/bc9e1aa8a6424ad3a335b8756a81c934")
+      },
+      network_id: 3,
+      gas:4612388,
+      gasPrice:1000000000000
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
